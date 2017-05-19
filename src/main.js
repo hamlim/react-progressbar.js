275ae7d8a63101030f9798f1fe380a39766d2557
Matt Hamlin <hamlim@rpi.edu>
2017-05-19 18:11:11 -0400

    Refactor main.js to use es6+ standards.
    Using react classes, stateless functional components, and es6 import+export.

--- src/main.js
+++ src/main.js
@@ -1,35 +1,23 @@
-var React = require('react');
-var ReactDom = require('react-dom');
-var isEqual = require('lodash.isequal');
-var ProgressBar = require('progressbar.js');
+import React, { Component } from 'react';
+import {isEqual} from 'lodash';
+import ProgressBar from 'progressbar.js';
 
-var Shape = React.createClass({
-    getDefaultProps: function getDefaultProps() {
-        return {
-            ShapeClass: null,
-            options: {},
-            progress: 0,
-            text: null,
-            initialAnimate: false,
-            containerStyle: {},
-            containerClassName: '.progressbar-container'
-        };
-    },
 
-    getInitialState: function getInitialState() {
-        return {
-            shape: null
-        };
-    },
+class Shape extends Component {
+    static defaultProps = {
+        ShapeClass: null,
+        options: {},
+        progress: 0,
+        text: null,
+        initalAnimate: false,
+        containerStyle: {},
+        containerClassName: '.progressbar-container'
+    }
+    state = {
+        shape: null
+    }
 
-    render: function render() {
-        var style = this.props.containerStyle;
-        var className = this.props.containerClassName;
-
-        return <div className={className} style={style} ref="progressBar"></div>;
-    },
-
-    componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+    componentWillRecieveProps(nextProps) {
         if (!isEqual(this.props.options, nextProps.options)) {
             this._destroy();
             this._create(nextProps, this.props);
@@ -38,24 +26,24 @@ var Shape = React.createClass({
 
         this._animateProgress(nextProps.progress);
         this._setText(nextProps.text);
-    },
+    }
 
-    componentDidMount: function componentDidMount() {
+    componentDidMount() {
         this._create(this.props);
-    },
+    }
 
-    componentWillUnmount: function componentWillUnmount() {
-        this._destroy()
-    },
+    componentWillUnmount() {
+        this._destroy();
+    }
 
-    _create: function _create(props, oldProps) {
+    _create = (props, oldProps) => {
         if (this.state.shape !== null) {
             throw new Error('Progressbar is already created');
         }
 
         // setState function is not used to prevent a new render cycle
         // This handling happens outside of React component's lifecycle
-        var container = ReactDom.findDOMNode(this.refs.progressBar);
+        const container = this.progressBar;
         this.state.shape = new props.ShapeClass(
             container,
             props.options
@@ -72,50 +60,43 @@ var Shape = React.createClass({
         }
 
         this._setText(props.text);
-    },
-
-    _destroy: function _destroy() {
+    }
+    
+    _destroy = () => {
         if (this.state.shape) {
             this.state.shape.destroy();
             this.state.shape = null;
         }
-    },
-
-    _animateProgress: function _animateProgress(progress) {
+    }
+    
+    _animateProgress = progress => {
         this.state.shape.animate(progress);
     },
 
-    _setProgress: function _setProgress(progress) {
+    _setProgress = progress => {
         this.state.shape.set(progress);
     },
 
-    _setText: function _setText(text) {
+    _setText = text => {
         if (text) {
             this.state.shape.setText(text);
         }
     }
-});
 
-var Line = React.createClass({
     render() {
-        return <Shape {...this.props} ShapeClass={ProgressBar.Line} />;
+        const { containerStyle: style, containerClassName: className } = this.props;
+        return <div className={className} style={style} ref={node => {this.progressBar = node; }} />;
     }
-});
+}
 
-var Circle = React.createClass({
-    render() {
-        return <Shape {...this.props} ShapeClass={ProgressBar.Circle} />;
-    }
-});
+const Line = props => <Shape {...props} ShapeClass={ProgressBar.Line} />;
 
-var SemiCircle = React.createClass({
-    render() {
-        return <Shape {...this.props} ShapeClass={ProgressBar.SemiCircle} />;
-    }
-});
+const Circle = props => <Shape {...props} ShapeClass={ProgressBar.circle} />;
 
-module.exports = {
-    Line: Line,
-    Circle: Circle,
-    SemiCircle: SemiCircle
+const SemiCircle = props => <Shape {...props} ShapeClass={ProgressBar.SemiCircle} />;
+
+export {
+    Line,
+    Circle,
+    SemiCircle
 };
